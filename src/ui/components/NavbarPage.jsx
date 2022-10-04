f246a0685034936ca138450f87473d1997622b53
Laura Valdez <laura.valdez@argano.com>
2022-10-04 13:16:07 -0600

    updates

--- src/ui/components/NavbarPage.jsx
+++ src/ui/components/NavbarPage.jsx
@@ -49,7 +49,7 @@ export const NavbarPage = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     
-                    <span className="nav-item nav-link text-primary">Laura</span>
+                    <span className="nav-item nav-link text-primary"> { user?.name }</span>
 
                     <button className="nav-item nav-link btn"
                      onClick={onLogout}>
